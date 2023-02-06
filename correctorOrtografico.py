aecf414f005369fc9b34b7057aeee13efc2fac40
Rodrigo Paredes <rodrigo.paredesp@userena.cl>
2023-02-06 15:57:46 -0300

    Version con Inventarios y valores de compra/venta vacios

--- correctorOrtografico.py
+++ correctorOrtografico.py
@@ -9,76 +9,127 @@ from tqdm import tqdm
 
 
 
-def agrega_inventarios(codigo):
 
-    inventario_laserena.append(inventariado_laserena(codigo))
-    inventario_industrial.append(inventariado_industrial(codigo))
-    inventario_matriz.append(inventariado_matriz(codigo))
-    inventario_bodega.append(inventariado_bodega(codigo))
     
 def inventariado_industrial(codigo):
 
+    codigo = int(codigo)
     inicio = 0
-    industrial = open ("./database/barrio_industrial.csv","r")
-    csvreader = csv.reader(industrial, delimiter=',')
-    for row in csvreader:
-       # if(inicio==3):
-            if(row[0]==codigo):
-                return row[5]
-
-        #else:
-         #  inicio = inicio + 1
-
-    industrial.close()
+   
+    if(codigo in codigos_industrial):
+        
+     #   input("Se encontro el codigo en bodega")
+        return inv_industrial[codigos_industrial.index(codigo)]
     return ""
 
 def inventariado_laserena(codigo):
 
+    codigo = int(codigo)
     inicio = 0
-    laserena = open ("./database/la serena.csv","r")
-    csvreader = csv.reader(laserena, delimiter=',')
-    for row in csvreader:
-       # if(inicio==3):
-            if(row[0]==codigo):
-
-                return row[5]
-        #else:
-         #  inicio = inicio + 1
-
-    laserena.close()
+   
+    if(codigo in codigos_laserena):
+        
+     #   input("Se encontro el codigo en bodega")
+        return inv_laserena[codigos_laserena.index(codigo)]
     return ""
 
 def inventariado_matriz(codigo):
 
+    codigo = int(codigo)
     inicio = 0
-    matriz = open ("./database/matriz.csv","r")
-    csvreader = csv.reader(matriz, delimiter=',')
-    for row in csvreader:
-       # if(inicio==3):
-            if(row[0]==codigo):
-
-                return row[5]
-        #else:
-         #  inicio = inicio + 1
-
-    matriz.close()
-    return ""  
+   
+    if(codigo in codigos_matriz):
+        
+        #input("Se encontro el codigo en matriz")
+        return inv_matriz[codigos_matriz.index(codigo)]
+    return "" 
 
 def inventariado_bodega(codigo):
+
+
+    codigo = int(codigo)
+    inicio = 0
+   
+    if(codigo in codigos_bodega):
+        
+    #    input("Se encontro el codigo en bodega")
+        return inv_bodega[codigos_bodega.index(codigo)]
+   # bodega = open ("./database/bodega.csv","r")
+   # csvreader = csv.reader(bodega, delimiter=',')
+   # for row in csvreader:
+    #    if(inicio==3):
+            # codigo_sucursal = int(row[0].replace(" ",""))
+           # if(codigo_sucursal==(codigo)):
+            #    return row[5]
+     #   else:
+      #     inicio = inicio + 1
+    
+  #  bodega.close()
+
+    
+    return "" 
+
+
+def guarda_codigos():
+
 
     inicio = 0
     bodega = open ("./database/bodega.csv","r")
     csvreader = csv.reader(bodega, delimiter=',')
     for row in csvreader:
-       # if(inicio==3):
-            if(row[0]==codigo):
-
-                return row[5]
-        #else:
-         #  inicio = inicio + 1
-
+        if(inicio==3):
+            codigo_sucursal = int(row[0].replace(" ",""))
+            codigos_bodega.append(codigo_sucursal)
+            inv_bodega.append(row[5])
+        else:
+           inicio = inicio + 1
     bodega.close()
-    return ""  
+
+    inicio = 0
+    laserena = open ("./database/la serena.csv","r")
+    csvreader = csv.reader(laserena, delimiter=',')
+    for row in csvreader:
+        if(inicio==3):
+            codigo_sucursal = int(row[0].replace(" ",""))
+            codigos_laserena.append(codigo_sucursal)
+            inv_laserena.append(row[5])
+        else:
+           inicio = inicio + 1
+    laserena.close()
+
+    inicio = 0
+    industrial = open ("./database/barrio_industrial.csv","r")
+    csvreader = csv.reader(industrial, delimiter=',')
+    for row in csvreader:
+        if(inicio==3):
+            codigo_sucursal = int(row[0].replace(" ",""))
+            codigos_industrial.append(codigo_sucursal)
+            inv_industrial.append(row[5])
+
+        else:
+           inicio = inicio + 1
+    industrial.close()
+
+    
+    inicio = 0
+    matriz = open ("./database/matriz.csv","r")
+    csvreader = csv.reader(matriz, delimiter=',')
+    for row in csvreader:
+        if(inicio==3):
+            codigo_sucursal = int(row[0].replace(" ",""))
+            codigos_matriz.append(codigo_sucursal)
+            inv_matriz.append(row[5])
+                
+        else:
+           inicio = inicio + 1
+    
+    matriz.close()
+
+
+
+
+  
+
 
 
 
@@ -95,26 +146,47 @@ inventario_laserena = []
 inventario_matriz = []
 inventario_bodega = []
 descripcion = []
-
-stock_industrial = []
-stock_laserena = []
-stock_matriz = []
-stock_bodega = []
-
 tipo_error = []
 
 
+codigos_industrial = []
+codigos_laserena = []
+codigos_matriz = []
+codigos_bodega = []
+
+inv_industrial = []
+inv_laserena = []
+inv_matriz = []
+inv_bodega = []
+
+
+
+
+
+
+
+
+contador = 0
+
+guarda_codigos()
+
 for row in csvreader:
 
+        # print(row[9])
+        # print(row[8])
         # print(row[1])
         
         #print(letra_descripcion)
         if(row[1]=='' or row[1].isspace()):
-            print("La descripcion esta vacia")
+           # print("La descripcion esta vacia")
             codigo.append(row[0])
             descripcion.append('Vacio')
             tipo_error.append("Descripcion vacia")
-            agrega_inventarios(row[0])
+            
+            inventario_laserena.append(inventariado_laserena(row[0]))
+            inventario_industrial.append(inventariado_industrial(row[0]))
+            inventario_matriz.append(inventariado_matriz(row[0]))
+            inventario_bodega.append(inventariado_bodega(row[0]))
         else:
     
             descripcion_sin_espacios = str(row[1]).replace(" ","")
@@ -122,32 +194,41 @@ for row in csvreader:
             #time.sleep(1)
             if(descripcion_sin_espacios.isdigit()):
                 
-                print("La descripcion es un numero", descripcion_sin_espacios, "Codigo: ",row[0])
+            #    print("La descripcion es un numero", descripcion_sin_espacios, "Codigo: ",row[0])
                 codigo.append(row[0])
                 descripcion.append(row[1])
                 tipo_error.append("Solo numeros")
-                agrega_inventarios(row[0])
+                inventario_laserena.append(inventariado_laserena(row[0]))
+                inventario_industrial.append(inventariado_industrial(row[0]))
+                inventario_matriz.append(inventariado_matriz(row[0]))
+                inventario_bodega.append(inventariado_bodega(row[0]))
 
             else:
                 descripcion_separada = str(row[1]).split()
                 #  print(descripcion_separada)
                 # print( len(descripcion_separada))
-                # if(descripcion_separada[0].isalpha() and len(descripcion_separada[0]) == 1 and len(descripcion_separada) == 2):
-                #     print("La descripcion empieza con una letra:", row[1])
-                #     codigo.append(row[0])
-                #     descripcion.append(row[1])
-                #     tipo_error.append("Empieza con letra")
-                #     agrega_inventarios(row[0])
+                if(descripcion_separada[0].isalpha() and len(descripcion_separada[0]) == 1 and len(descripcion_separada) == 2 and ((row[9] == '' and row[11] == '') or (row[9] != '' and row[11] == '') or (row[9] == '' and row[11] != ''))):
+                    #print("La descripcion empieza con una letra:", row[1])
+                    codigo.append(row[0])
+                    descripcion.append((row[1]).rstrip())
+                    tipo_error.append("Empieza con letra")
+                    inventario_laserena.append(inventariado_laserena(row[0]))
+                    inventario_industrial.append(inventariado_industrial(row[0]))
+                    inventario_matriz.append(inventariado_matriz(row[0]))
+                    inventario_bodega.append(inventariado_bodega(row[0]))
                     
-
-
+        contador+=1
+   #     print("dato",contador)
             
 
 maestra.close()
 
+
+
 palabras_vacias = pandas.DataFrame(list(zip(codigo,tipo_error,descripcion,inventario_industrial,inventario_laserena,inventario_matriz,inventario_bodega)), columns =["Codigo",'Tipo error', 'Descripcion',"I.Industrial","I.La Serena","I.Matriz","I.Bodega"])
-palabras_vacias.to_csv(r'C:\Users\Rodrigo', header=None, index=None, sep=' ', mode='a')
+palabras_vacias.to_csv(r'./errores.csv', header={"Codigo",'Tipo error', 'Descripcion',"I.Industrial","I.La Serena","I.Matriz","I.Bodega"}, index=False, sep=',', mode='w')
 #palabras_empieza_numero = pandas.DataFrame(list(zip(codigo_palabra_empieza_numero,descripcion_palabra_empieza_numero,inventario_industrial,inventario_laserena)), columns =["Codigo", 'Descripcion',"Inventario Industrial","Inventario La Serena"])
 print("PRODUCTOS ERRONEOS: \n")
-print(tabulate(palabras_vacias, headers = 'keys', tablefmt = 'psql'))
+table= tabulate(palabras_vacias, headers = 'keys', tablefmt = 'psql')
+print(table)
 #print("Palabras que empiezan con numero: \n",tabulate(palabras_empieza_numero, headers = 'keys', tablefmt = 'github'))
